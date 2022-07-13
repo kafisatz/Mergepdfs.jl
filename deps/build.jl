71f1d7614331da3f1186d0caf593383ef734bb91
Bernhard König <github@diro.ch>
2022-07-13 11:04:44 +0200

    pin pypdf 1.27.9

---
+++ deps/build.jl
@@ -0,0 +1,28 @@
+using Pkg
+
+Pkg.build("PyCall")
+Pkg.build("Conda")
+
+using Conda
+using PyCall
+
+pyModulePyPDF2 = PyCall.PyNULL()
+
+pypdf_version ="1.27.9"
+@warn("Removing PyPDF package and installing version $(pypdf_version)")
+#https://anaconda.org/conda-forge/pypdf2
+run(`pip uninstall PyPDF2 -y`)
+run(`pip install PyPDF2==$pypdf_version`)
+
+try 
+    #this line may trigger installation
+    copy!(pyModulePyPDF2, PyCall.pyimport("PyPDF2"))    
+catch e 
+    @show e 
+    try 
+        copy!(pyModulePyPDF2, PyCall.pyimport_conda("PdfFileMerger","PyPDF2","conda-forge"))
+    catch e 
+        @show e 
+    end    
+end 
+    
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
 #
 #]activate .
+#using Pkg;Pkg.build("Mergepdfs")
 using Test
-using Mergepdfs
 using PyCall
+using Mergepdfs
 
 datafld = first(filter(isdir,["samplepdfs","../samplepdfs"]))
 fi1 = joinpath(datafld,"1.pdf")
@@ -13,18 +14,21 @@ fi2 = joinpath(datafld,"2.pdf")
 tmpfld = mktempdir()
 @assert isdir(tmpfld)
 
-
 list_of_pdf_files = joinpath.(datafld,["1.pdf","2.pdF","3.PDF","4.pDf"])
 @assert all(isfile.(list_of_pdf_files))
 
 @testset "Smoke Tests" begin
 
+#if this is equal, initialiation failed
+@test pyModulePyPDF2 != PyCall.PyNULL 
+    
 intermediatePdfFile = joinpath(tmpfld,"intermediate.pdf")
 outputPdfFile = joinpath(tmpfld,"res.pdf")
 
 ##################################################################
 #merging pdf files
 ##################################################################
+
 merger = pyModulePyPDF2.PdfFileMerger()
 
 merger.append(fi1)
