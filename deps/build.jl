f8f331a2cf9a4340bfa1e5111c3fbcca53911088
Bernhard König <github@diro.ch>
2022-09-23 13:44:09 +0200

    edt

--- deps/build.jl
+++ deps/build.jl
@@ -9,20 +9,25 @@ using PyCall
 pyModulePyPDF2 = PyCall.PyNULL()
 
 pypdf_version="1.27.9"
-@warn("Removing PyPDF package and installing version $(pypdf_version)")
-#https://anaconda.org/conda-forge/pypdf2
-run(`pip uninstall PyPDF2 -y`)
-run(`pip install PyPDF2==$pypdf_version`)
-
 try 
-    #this line may trigger installation
-    copy!(pyModulePyPDF2, PyCall.pyimport("PyPDF2"))    
-catch e 
-    @show e 
+    @warn("Removing PyPDF package and installing version $(pypdf_version)")
+    #https://anaconda.org/conda-forge/pypdf2
+    run(`pip uninstall PyPDF2 -y`)
+    run(`pip install PyPDF2==$pypdf_version`)
+
     try 
-        copy!(pyModulePyPDF2, PyCall.pyimport_conda("PdfFileMerger","PyPDF2","conda-forge"))
+        #this line may trigger installation
+        copy!(pyModulePyPDF2, PyCall.pyimport("PyPDF2"))    
     catch e 
         @show e 
-    end    
+        try 
+            copy!(pyModulePyPDF2, PyCall.pyimport_conda("PdfFileMerger","PyPDF2","conda-forge"))
+        catch e 
+            @show e 
+        end    
+    end 
+        
+catch ee 
+    @warn("failed to build MergePDFs")
+    @show ee
 end 
-    
